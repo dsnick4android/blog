03afc3b39fe22810e95cc39992211f93e256e671
dsnick <ds.mail.ru@gmail.com>
2019-01-29 20:31:54 +0300

    lesson-2-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'about/me'
+  get 'home/index'
+  get 'home/about'
+  get 'home/portfolio'
+  get 'home/contact'
+
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
